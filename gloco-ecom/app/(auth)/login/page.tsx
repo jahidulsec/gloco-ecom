ea1362c8d5743583b732bd7af529fe60bacb64ec
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-06 18:36:17 +0600

    feat: adding meta icon

--- gloco-ecom/app/(auth)/login/page.tsx
+++ gloco-ecom/app/(auth)/login/page.tsx
@@ -1,6 +1,11 @@
 import LoginBg from "@/components/assets/login/LoginBg";
 import LoginSection from "@/components/login/LoginSection";
 import React from "react";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Login - Glocom',
+}
 
 function LoginPage() {
   return (
---
+++ gloco-ecom/app/icon.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: "transparent",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "#1d4ed8",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-audio-waveform"
+        >
+          <path d="M2 13a2 2 0 0 0 2-2V7a2 2 0 0 1 4 0v13a2 2 0 0 0 4 0V4a2 2 0 0 1 4 0v13a2 2 0 0 0 4 0v-4a2 2 0 0 1 2-2" />
+        </svg>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+}
